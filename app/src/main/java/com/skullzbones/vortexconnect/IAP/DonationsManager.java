295b4fd3de9370f4bc14e211ef4dfc145b86755b
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-09-04 00:36:28 +0530

    Russian Trans + Donate

---
+++ app/src/main/java/com/skullzbones/vortexconnect/IAP/DonationsManager.java
@@ -0,0 +1,55 @@
+package com.skullzbones.vortexconnect.IAP;
+
+import android.app.Activity;
+import android.content.Context;
+import android.widget.Toast;
+import com.limerse.iap.DataWrappers;
+import com.limerse.iap.IapConnector;
+import com.limerse.iap.PurchaseServiceListener;
+import com.skullzbones.vortexconnect.R;
+import com.skullzbones.vortexconnect.Utils.ToastUtils;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import org.jetbrains.annotations.NotNull;
+
+public class DonationsManager {
+  Context context;
+  IapConnector iapConnector;
+  List<String> consumablesList = Arrays.asList(
+      "vortex_donate_3"
+  );
+
+  public DonationsManager(Context context){
+    this.context = context;
+
+    iapConnector = new IapConnector(
+        context,
+        Collections.emptyList(),
+        consumablesList,
+        Collections.emptyList(),
+        context.getString(R.string.google_play_app_license_key),
+        true
+    );
+
+    iapConnector.addPurchaseListener(new PurchaseServiceListener() {
+      public void onPricesUpdated(@NotNull Map iapKeyPrices) {
+
+      }
+
+      public void onProductPurchased(DataWrappers.PurchaseInfo purchaseInfo) {
+        ToastUtils.out(context, R.string.thanks_for_donations);
+      }
+
+      public void onProductRestored(DataWrappers.PurchaseInfo purchaseInfo) {
+
+      }
+    });
+  }
+
+  public void donateIndex(Activity activity, int index){
+    iapConnector.purchase(activity, consumablesList.get(index));
+  }
+
+}
--- app/src/main/java/com/skullzbones/vortexconnect/ui/clans/ClansFragment.java
+++ app/src/main/java/com/skullzbones/vortexconnect/ui/clans/ClansFragment.java
@@ -1,5 +1,7 @@
 package com.skullzbones.vortexconnect.ui.clans;
 
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -12,12 +14,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.skullzbones.vortexconnect.IAP.DonationsManager;
 import com.skullzbones.vortexconnect.R;
+import org.jetbrains.annotations.NotNull;
 
 public class ClansFragment extends Fragment {
 
     private ClansViewModel mViewModel;
-
+    private DonationsManager donationsManager;
     public static ClansFragment newInstance() {
         return new ClansFragment();
     }
@@ -29,10 +33,22 @@ public class ClansFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onViewCreated(@NonNull @NotNull View view,
+        @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
         mViewModel = new ViewModelProvider(this).get(ClansViewModel.class);
         // TODO: Use the ViewModel
+
+        donationsManager = new DonationsManager(getContext());
+
+        Button m1 = getActivity().findViewById(R.id.button_donate_3);
+        m1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                donationsManager.donateIndex(getActivity(), 0);
+            }
+        });
     }
 
 }
